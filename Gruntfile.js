74a7ac7e1ed712364d4dde787f7eda56f7a3b34b
Hack Reactor Student <student@hackreactor.com>
2017-06-09 19:19:31 -0500

    Add Gruntfile etc

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,16 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
+      options: {
+        // define a string to put between each file in the concatenated output
+        separator: ';'
+      },
+      dist: {
+        // the files to concatenate
+        src: ['public/**/*.js'],
+        // the location of the resulting JS file
+        dest: 'dist/<%= pkg.name %>.js'
+      }
     },
 
     mochaTest: {
@@ -21,11 +31,21 @@ module.exports = function(grunt) {
     },
 
     uglify: {
+      options: {
+        // the banner is inserted at the top of the output
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
     },
 
     eslint: {
       target: [
         // Add list of files to lint here
+        'Gruntfile.js', 'public/**/*.js'
       ]
     },
 
@@ -53,6 +73,36 @@ module.exports = function(grunt) {
       prodServer: {
       }
     },
+    gitadd: {
+      task: {
+        options: {
+          all: true
+        },
+        files: {
+          src: ['public/**/*.js']
+        }
+      }
+    },
+    gitcommit: {
+      yourTarget: {
+        options: {
+          // Target-specific options go here.
+          message: 'Commit from grunt'
+        },
+        files: {
+            // Specify the files you want to commit
+          src: ['public/**/*.js']
+        }
+      }
+    },
+    gitpush: {
+      yourTarget: {
+        options: {
+          // Target-specific options go here.
+          remote: 'live'
+        }
+      }
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -63,6 +113,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-git');
 
   grunt.registerTask('server-dev', function (target) {
     grunt.task.run([ 'nodemon', 'watch' ]);
@@ -82,14 +133,18 @@ module.exports = function(grunt) {
   grunt.registerTask('upload', function(n) {
     if (grunt.option('prod')) {
       // add your production server task here
+      ['deploy'];
     } else {
       grunt.task.run([ 'server-dev' ]);
     }
   });
 
-  grunt.registerTask('deploy', [
-    // add your deploy tasks here
-  ]);
+  grunt.registerTask('deploy', function(n) {
+    if(grunt.option('prod')) {
+      ['gitadd', 'gitcommit', 'gitpush'];
+    }
+    grunt.task.run(['concat', 'uglify', 'eslint', 'mochaTest', 'server-dev']);
+  });
 
 
 };
